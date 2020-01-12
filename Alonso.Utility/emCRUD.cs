34b9e7f4e1b45685d0606a9beabc05f1124bdb9c
Alonso Chen <mepowerlmay@gmail.com>
2020-01-12 17:51:22 +0800

    20200112

---
+++ Alonso.Utility/emCRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alonso.Utility
+{
+    public  enum emCRUD
+    {
+        /// <summary>
+        /// 顯示
+        /// </summary>
+        View,
+        /// <summary>
+        /// 編輯
+        /// </summary>
+        Edit ,
+       /// <summary>
+       /// 新增
+       /// </summary>
+        Add
+    }
+}
